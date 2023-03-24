92556a9a900f4e16934d6aaa4019b71d0720ea97
Ilona001 <ilona_mikhaylenko@hotmail.com>
2023-03-24 13:18:24 -0400

    Script to download stock prices

--- notebook.py
+++ notebook.py
@@ -1,3 +1,3 @@
-
-print('Hello World')
-print('How are you')
+import yfinance as yf
+ticks = ["JPM", "BAC", "HSBC", "WFC", "RY", "TD", "C", "MUFG", "BMO", "UBS", "BNS", "SAN", "SMFG", "ING", "BBVA", "CM", "NWG", "BCS", "NU", "EWBC", "CS", "NTB", "SIVB", "SBNY"]
+yf.download(ticks, start = "2020-03-01", end = "2023-03-31")["Adj Close"]
